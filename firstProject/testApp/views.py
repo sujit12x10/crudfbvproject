b2e1aa7779152a299ab789c33307056ba6861c79
sujit12x10 <124373923+sujit12x10@users.noreply.github.com>
2023-02-05 12:47:07 +0530

    Delete firstProject directory

--- firstProject/testApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def welcome(request):
-    s = '<h1> Hello Welcome to Django Classes. Well Done TONI</h1>'
-    return HttpResponse(s)
-
-# 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
